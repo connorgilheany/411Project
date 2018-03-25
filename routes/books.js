e0107549e7521d67d7b88222877abc77a4e3bd51
connorgilheany <connorgilheany@gmail.com>
2018-03-25 16:03:35 -0400

    Created a button on the home page that redirects to the books page

--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-    { title: 'Couch Surf',
-      booksURL: '/books' });
+  res.render('books', { title: 'Couch Surf' });
 });
 
 module.exports = router;
