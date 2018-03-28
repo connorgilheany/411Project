d29478e23dcf3b053d5b4b633c658ffb162e435d
Lawrence Luo <luolawrence1@gmail.com>
2018-03-28 18:08:30 -0400

    added list of results, code cleanup

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var request = require('request');
-var url = 'https://www.googleapis.com/books/v1/volumes?q=hacker+terms';
-var bookTitle;
 
+// Returns promise with list of book titles if request is successful
 let searchBooks = (queryString) => {
   return new Promise((resolve, reject) => {
     request('https://www.googleapis.com/books/v1/volumes?q=' + queryString, (error, response, body) => {
       if (!error && response.statusCode == 200) {
-        ja = JSON.parse(body);
-        bookTitle = ja.items[0].volumeInfo.title; // "Food"
+        let jsonBody = JSON.parse(body);
+        let bookTitles = [];
 
-        jj = ja.totalItems;
-        console.log(jj); // gives undefined
-        console.log(bookTitle);
-        resolve(bookTitle);
+        // grab first 10 results
+        jsonBody.items.slice(0,10).forEach(item => {
+          bookTitles.push(item.volumeInfo.title);
+        });
+
+        resolve(bookTitles);
       } else {
         reject(Error("No Book Returned Yet"));
       }
@@ -25,28 +26,21 @@ let searchBooks = (queryString) => {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-/*  res.render('index', 
-    { title: 'Couch Surf',
-      booksURL: '/books',
-      watchURL: '/watch'  }); */
-  res.render('index', { title: 'Couch Surf' });
+  res.render('index', { title: 'Couch Surf', results: [] });
 });
 
 router.post('/search', function(req, res, next) {
-	
-	// req.checkBody('search-books', 'Must be something in search box').notEmpty();
-	var input = req.body.title; // queryString = ''
+	var input = req.body.query;
 	searchBooks(input).then((books) => {
     res.render('index',
       {
         booksURL: '/books',
         watchURL: '/watch',
         title: 'COUCH SURF',
-        result1: 'Here are the results!',
-		result2: books 
+        results: books,
+        resultsLength: books.length + ' results found'
       });
   });
 });
 
 module.exports = router;
-
