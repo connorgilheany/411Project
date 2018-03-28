c15dda5432343ab7645e0e58d9909aa3f92ac6ea
Tkixi <tkixi@bu.edu>
2018-03-28 17:34:56 -0400

    demo-app, needs more than just one result

--- routes/index.js
+++ routes/index.js
@@ -3,40 +3,49 @@ var router = express.Router();
 
 var request = require('request');
 var url = 'https://www.googleapis.com/books/v1/volumes?q=hacker+terms';
+var bookTitle;
 
-var jj;
+let searchBooks = (queryString) => {
+  return new Promise((resolve, reject) => {
+    request('https://www.googleapis.com/books/v1/volumes?q=' + queryString, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        ja = JSON.parse(body);
+        bookTitle = ja.items[0].volumeInfo.title; // "Food"
 
-var books = request('https://www.googleapis.com/books/v1/volumes?q=food+terms', function (error, response, body) {
- // console.log(response)
-  if (!error && response.statusCode == 200) {
-  	ja = JSON.parse(body);
-  	jj = ja.totalItems;
-    console.log(jj); // gives undefined
-    return jj
-  }
-});
-
-console.log(jj);
-
-//console.log(title)
-/* GET book search */
-
-//var result = request('https://www.googleapis.com/books/v1/volumes?q=hacker+terms', { json: true }, (err, res, body) => {
-//  if (err) { return console.log(err); }
- // console.log(body.url);
- // console.log(body.explanation);
- // console.log(result)
-//});
-//var parsed = result.items[0].volumeInfo.title;
-
+        jj = ja.totalItems;
+        console.log(jj); // gives undefined
+        console.log(bookTitle);
+        resolve(bookTitle);
+      } else {
+        reject(Error("No Book Returned Yet"));
+      }
+    });
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
+/*  res.render('index', 
     { title: 'Couch Surf',
       booksURL: '/books',
-      watchURL: '/watch'  });
-  res.render('index', { title: 'Couch Surf', book: parsed });
+      watchURL: '/watch'  }); */
+  res.render('index', { title: 'Couch Surf' });
+});
+
+router.post('/search', function(req, res, next) {
+	
+	// req.checkBody('search-books', 'Must be something in search box').notEmpty();
+	var input = req.body.title; // queryString = ''
+	searchBooks(input).then((books) => {
+    res.render('index',
+      {
+        booksURL: '/books',
+        watchURL: '/watch',
+        title: 'COUCH SURF',
+        result1: 'Here are the results!',
+		result2: books 
+      });
+  });
 });
 
 module.exports = router;
