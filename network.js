d39327abfc09a4f0fe3640db50f19abaf1798fbf
Tkixi <tkixi@bu.edu>
2018-04-24 19:36:49 -0400

    working cache

--- network.js
+++ network.js
@@ -16,26 +16,57 @@ console.log(defaultApp.name);  // "[DEFAULT]"
 
 var defaultDatabase = defaultApp.database();
 
-function writeBookData(url, parsedResult) {
-  firebase.database().ref('books/').child('user/').set({ //insert specific uid
+
+function writeBookData(url, parsedResult, queryString) {
+  firebase.database().ref('cache/').child("insertUID").child(queryString).update({ //insert specific uid
     "url": url,
     "result": parsedResult
   });
 }
 
 //Returns a promise with the result of the cache lookup / network call
-let call = (url, parser) => {
+let call = (url, parser, queryString) => {
   return new Promise((resolve, reject) => {
     //Check if it's in the cache (URL key), if so resolve with the data from cache
     
-    // connection((db) => {
-    //   let cachedItem = db.collection('cached').findOne({"url": url});
-    //   console.log(cachedItem);
-    //     if(cachedItem) {
-    //       console.log(cachedItem.result);
-    //       resolve(cachedItem.result);
-    //     }
-    //   });
+    var query = firebase.database().ref('cache/').child("insertUID");
+    query.once("value") 
+      .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var key = childSnapshot.key;
+          console.log("this key" + key);
+          if (key == queryString){
+            console.log("Got cache");
+            resolve(childSnapshot.child("result").val());
+          }
+          else{
+            request(url, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+              let jsonBody = JSON.parse(body);
+              let parsed = parser(jsonBody);
+              //Add it to the cache with the URL as the key
+              resolve(parsed);
+              writeBookData(url, parsed, queryString);
+            } else {
+              reject(Error("Error processing request to URL: "+parser));
+            }
+          });
+
+          }
+        })
+      });
+    // ref.on("value", function(snapshot) {
+    //   var storedQuery = snapshot.child("insertUID/").val();
+    //   if (storedQuery != null) {
+    //     for 
+    //   }
+
+    //    console.log(snapshot.val());
+    // }, function (error) {
+    //    console.log("Error: " + error.code);
+    // });
+
+    // ref.off("value");
 
     // if query in database
     // return query content in db
@@ -45,24 +76,19 @@ let call = (url, parser) => {
     //
 
     
-    request(url, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        let jsonBody = JSON.parse(body);
-        let parsed = parser(jsonBody);
-        //Add it to the cache with the URL as the key
-        resolve(parsed);
-        writeBookData(url, parsed);
+    // request(url, (error, response, body) => {
+    //   if (!error && response.statusCode == 200) {
+    //     let jsonBody = JSON.parse(body);
+    //     let parsed = parser(jsonBody);
+    //     //Add it to the cache with the URL as the key
+    //     resolve(parsed);
+    //     writeBookData(url, parsed, queryString);
 
 
-    //     connection((db) => {
-    //     db.collection('cached')
-    //         .insert({"url": url, "result": parsed})
+    //   } else {
+    //     reject(Error("Error processing request to URL: "+parser));
+    //   }
     // });
-
-      } else {
-        reject(Error("Error processing request to URL: "+parser));
-      }
-    });
 
 
   });
