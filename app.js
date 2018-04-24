2226c9c3df54b7ddd742f7935721f77da5e825ec
Tkixi <tkixi@bu.edu>
2018-04-24 18:32:48 -0400

    removed api.js from code

--- app.js
+++ app.js
@@ -26,13 +26,42 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', booksRouter);
 app.use('/watch', watchRouter);
-app.use('/api', api);
 
+
+var userService = require('./user_service');
+
+//Create new user
+app.post('/user', function(req, res) {
+	var newUserEmail = req.headers['user-email'];
+	var newUserPass = req.headers['user-pass'];
+	userService.addUser(newUserEmail, newUserPass, 
+		function(error, uid) {
+			if (error) {
+				return res.status(500).send('Error when creating user');
+			} else {			
+				return res.status(201).send({uid : uid});
+		}
+	});
+});
+
+//Perform authentication
+app.post('/login', function(req, res){
+	var userEmail = req.headers['user-email'];
+	var userPassword = req.headers['user-pass'];
+	userService.authenticate(userEmail, userPassword,
+		function(error, authData) {
+			if (error) {
+				return res.status(401).send('Unauthorized');
+			} else {
+				return res.status(200).send(authData);
+		}
+	});
+});
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
