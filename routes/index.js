09cde133f7ee58c0ce860d5d946117f313f52447
Tkixi <tkixi@bu.edu>
2018-04-24 22:13:12 -0400

    signout functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase');
+const firebaseConfig = require('../config');
+
+var defaultApp = firebase.initializeApp(firebaseConfig);
+
+var firebase = require('firebase');
+var user = firebase.auth().currentUser;
 
 
 /* GET home page. */
@@ -10,6 +17,29 @@ router.get('/', function(req, res, next) {
     watchURL: '/watch',
     signupURL: '/signup', 
     signinURL: '/signin'});
+});
+
+
+router.get('/signout', function(req, res, next) {
+	firebase.auth().signOut().then(function() {
+	  console.log('Signed Out');
+	  res.render('index', { 
+	    title: 'Couch Surf', 
+	    booksURL: '/books',
+	    watchURL: '/watch',
+	    signupURL: '/signup', 
+	    signinURL: '/signin'});
+
+	  // debug for if user signs out
+	  firebase.auth().onAuthStateChanged( user => {
+	    if (user) { 
+	      this.userId = user.uid;
+	      console.log(user.uid); 
+	    }
+	  });
+	}, function(error) {
+	  console.error('Sign Out Error', error);
+	});
 });
 
 
